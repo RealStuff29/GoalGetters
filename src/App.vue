e8aaee7147cb127683626d99cf5ee3665af26394
RealStuff29 <mikhailghazi2001@gmail.com>
2025-10-09 15:22:44 +0800

    This is a github push test
    Test

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div>
     <nav class="navbar bg-light px-3">
       <div class="d-flex">
-      <router-link to="/" class="nav-link mx-3">Home</router-link>
-      <router-link to="/login" class="nav-link mx-3">Login</router-link>
-      <router-link to="/crudview" class="nav-link mx-3">Crud Test</router-link>
+        <router-link to="/" class="nav-link mx-3">Home</router-link>
+        <router-link to="/login" class="nav-link mx-3">Login</router-link>
+        <router-link to="/crudview" class="nav-link mx-3">Crud Test</router-link>
 
       </div>
     </nav>
