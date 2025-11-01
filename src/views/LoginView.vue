09bc21706d8cf71bb51114a69cae45aeaa2e04fd
RealStuff29 <mikhailghazi2001@gmail.com>
2025-11-01 17:39:20 +0800

    Added regex to login and register pages, as well as fixed minor styling issue with registerview

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -13,7 +13,15 @@ import router from '@/router';
 
 const { loginUser, loginUserWithGoogle, error, loading } = useAuth();
 
+const SMU_EMAIL = /^[A-Za-z0-9._%+-]+@smu\.edu\.sg$/i;
+
+
 async function handleLogin() {
+
+    if (!SMU_EMAIL.test(email.value)) {
+        alert('Please log in with your SMU email (must end with @smu.edu.sg).');
+        return;
+    }
 
     await loginUser(email.value, password.value);
 
@@ -43,27 +51,29 @@ async function handleGoogleLogin() {
                 <img v-bind:src="logo" class="img-fluid w-50">
 
                 <div class="mt-3 w-75">
-                <form @submit.prevent="handleLogin()">
+                    <form @submit.prevent="handleLogin()">
 
-                    <div class="mb-3">
-                        <label for="email">Email</label>
-                        <InputText type="email" v-model="email" fluid />
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="password">Password</label>
-                        <InputText type="password" v-model="password" fluid />
-                        <div class="form-text">
-                            New to GoalGetters?
-                            <RouterLink v-bind:to="{ name: 'register' }" class="fw-medium text-decoration-none">
-                                Sign up!
-                            </RouterLink>
+                        <div class="mb-3">
+                            <label for="email">Email</label>
+                            <InputText type="email" v-model="email" fluid pattern="^[A-Za-z0-9._%+-]+@smu\.edu\.sg$"
+                                @invalid="e => e.target.setCustomValidity('Please use your SMU email (must end with @smu.edu.sg).')"
+                                @input="e => e.target.setCustomValidity('')" />
                         </div>
-                    </div>
-                    <Button type="submit" label="Submit" />
-                </form>
 
-                
+                        <div class="mb-3">
+                            <label for="password">Password</label>
+                            <InputText type="password" v-model="password" fluid />
+                            <div class="form-text">
+                                New to GoalGetters?
+                                <RouterLink v-bind:to="{ name: 'register' }" class="fw-medium text-decoration-none">
+                                    Sign up!
+                                </RouterLink>
+                            </div>
+                        </div>
+                        <Button type="submit" label="Submit" />
+                    </form>
+
+
                     <hr class="my-3" />
                     <button class="btn btn-outline-dark w-100 d-flex align-items-center justify-content-center gap-2"
                         type="button" @click="handleGoogleLogin" v-bind:disabled="loading">
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -13,54 +13,75 @@ import router from '@/router';
 
 const { registerUser, error, loading } = useAuth();
 
+const SMU_EMAIL = /^[A-Za-z0-9._%+-]+@smu\.edu\.sg$/i;
+
+
+// OLD HANDLE REGISTER (PRE-REGEX)
+// async function handleRegister() {
+//     await registerUser(email.value, password.value);
+
+//     if (error.value) {
+//         alert(error.value);
+//     } else {
+//         alert('Account created!');
+
+//         router.push('/profilesetupview');
+//     }
+// }
+
 async function handleRegister() {
-
-    //ADD LOGIC TO CHECK FOR EMPTY STUFF LATER
-
-    //ALSO, MAKE USE OF "loading" TO TELL THE USER THEIR REQUEST IS LOADING!
+    if (!SMU_EMAIL.test(email.value)) {
+        alert('Please use your SMU email (must end with @smu.edu.sg).');
+        return;
+    }
 
     await registerUser(email.value, password.value);
 
     if (error.value) {
         alert(error.value);
-    } else {
-        alert('Account created!');
-
-        //LINK to QL VIEW
-        router.push('/profilesetupview');
+        return;
     }
+
+    router.push('/profilesetupview');
 }
+
+
 </script>
 
 <template>
     <div class="container-fluid vh-100">
         <div class="row h-100">
-            <!-- THIS VERSION IS BETTER COMPARED TO MAIN AS I FIXED COLUMNS -->
-            <div class="col-4 d-flex flex-column align-items-center justify-content-center shadow-lg m-0 p-0">
+            <div class="col-4 d-flex flex-column align-items-center justify-content-center shadow-lg m-0 p-0 com-md-12">
 
                 <img v-bind:src="logo" class="img-fluid w-50">
 
-                <form @submit.prevent="handleRegister()">
+                <div class="mt-3 w-75">
+                    <form @submit.prevent="handleRegister()">
 
-                    <div class="mb-3">
-                        <label for="email">Email</label>
-                        <InputText type="email" v-model="email" fluid />
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="password">Password</label>
-                        <InputText type="password" v-model="password" fluid />
-                        <div class="form-text">
-                            Already have an account?
-                            <RouterLink v-bind:to="{name:'login'}" class="fw-medium text-decoration-none">
-                                Log in!
-                            </RouterLink>
+                        <div class="mb-3">
+                            <label for="email">Email</label>
+                            <!-- <InputText type="email" v-model="email" fluid /> -->
+                            <InputText type="email" v-model="email" fluid pattern="^[A-Za-z0-9._%+-]+@smu\.edu\.sg$"
+                                @invalid="e => e.target.setCustomValidity('Please use your SMU email (must end with @smu.edu.sg).')"
+                                @input="e => e.target.setCustomValidity('')" />
+                            <div class="form-text">Use your SMU email (must end with @smu.edu.sg).</div>
                         </div>
-                    </div>
-                    
-                    <Button type="submit" label="Submit" />
 
-                </form>
+                        <div class="mb-3">
+                            <label for="password">Password</label>
+                            <InputText type="password" v-model="password" fluid />
+                            <div class="form-text">
+                                Already have an account?
+                                <RouterLink v-bind:to="{ name: 'login' }" class="fw-medium text-decoration-none">
+                                    Log in!
+                                </RouterLink>
+                            </div>
+                        </div>
+                        <Button type="submit" label="Submit" />
+                    </form>
+                </div>
+
+
 
             </div>
             <div class="col-8 bg-dark p-0 h-100 overflow-hidden position-relative">
